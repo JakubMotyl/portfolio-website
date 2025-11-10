c9eadedd057f50a12e52e7ac55a307b735ae06ff
Jakub Motyl <kubamotyl16@gmail.com>
2025-11-10 16:55:13 +0100

    project setup

---
+++ src/components/Hero.tsx
@@ -0,0 +1,7 @@
+export default function Hero() {
+  return (
+    <section className="h-[calc(100vh-5rem)] bg-[#212121]">
+      <p>elo</p>
+    </section>
+  );
+}
---
+++ src/components/Navbar.tsx
@@ -0,0 +1,7 @@
+export default function Navbar() {
+  return (
+    <nav className="bg-[#1a1a1a] h-20 px-25 border-b border-b-[#F6F6F6] text-[#F6F6F6] navbar">
+      <div>giga emejzing navbar</div>
+    </nav>
+  );
+}
