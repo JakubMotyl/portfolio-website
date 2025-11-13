280260d5ddf38a1340b72b689bb4467f09f3d8c1
Jakub Motyl <kubamotyl16@gmail.com>
2025-11-13 19:43:11 +0100

    Finish footer

---
+++ src/components/Footer.tsx
@@ -0,0 +1,32 @@
+import { CiMail } from "react-icons/ci";
+import { SiLinkedin } from "react-icons/si";
+
+export default function Footer() {
+  return (
+    <footer className="md:text-sm text-xs section-p py-7  flex items-center justify-between max-w-6xl w-full mx-auto">
+      <div className="flex items-center justify-center gap-4">
+        <div className="flex items-center gap-1">
+          <SiLinkedin className="md:w-5 md:h-5 w-4 h-4 text-[#0A66C2]" />
+          <a
+            href="https://www.linkedin.com/in/jakub-motyl-141b13253/"
+            target="_blank"
+            rel="noopener noreferrer"
+            className="text-[#d8d8d8] hover:underline duration-200"
+          >
+            LinkedIn
+          </a>
+        </div>
+        <div className="flex items-center gap-1">
+          <CiMail className="md:w-5 md:h-5 w-4 h-4 text-[#FFF]" />
+          <a
+            href="mailto:kubamotyl16@gmail.com"
+            className="text-[#d8d8d8] hover:underline duration-200"
+          >
+            kubamotyl16@gmail.com
+          </a>
+        </div>
+      </div>
+      <p className="text-[#d8d8d8]">© 2025 Jakub Motyl</p>
+    </footer>
+  );
+}
